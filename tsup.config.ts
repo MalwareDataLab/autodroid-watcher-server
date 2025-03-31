3a80f67c8f95fb57a538ef30371f03424074d0c2
luizfelipelaviola <luiz@laviola.dev>
2025-03-30 21:23:59 -0300

    fix: set all deps to external on build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig(options => ({
   entry: ["./src/index.ts"],
   format: ["cjs"],
-  noExternal: [ /(.*)/ ],
   dts: false,
   splitting: false,
   sourcemap: true,
